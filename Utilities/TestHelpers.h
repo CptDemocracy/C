c6da4a50010b624dd2949726a97b2e66c94323fc
Efim Bogdanovsky <profit234@gmail.com>
2016-10-25 01:44:27 +0300

    Update TestHelpers.h

--- Utilities/TestHelpers.h
+++ Utilities/TestHelpers.h
@@ -11,3 +11,13 @@ do {                                                 \
     assert(lhs[index] == rhs[index]);                \
   }                                                  \
 } while (0)
+
+#define ASSERT_ARRAY_EQUALS_COMPARE_FN(lhs, rhs, cmp)\
+do {                                                 \
+  size_t lhsSize = ARRAY_SIZE(lhs);                  \
+  size_t rhsSize = ARRAY_SIZE(rhs);                  \
+  assert(lhsSize == rhsSize);                        \
+  for (size_t index = 0; index < lhsSize; ++index) { \
+    assert(!cmp(lhs[index], rhs[index]));            \
+  }                                                  \
+} while (0)
