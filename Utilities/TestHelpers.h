79af99bbe4acac53bbf35ac3ff927c94f8f96af2
Efim Bogdanovsky <profit234@gmail.com>
2016-10-25 01:37:13 +0300

    Create TestHelpers.h

---
+++ Utilities/TestHelpers.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+
+#define ARRAY_SIZE(arr) sizeof(arr)/sizeof(arr[0])
+
+#define ASSERT_ARRAY_EQUALS(lhs, rhs, ...)           \
+do {                                                 \
+  size_t lhsSize = ARRAY_SIZE(lhs);                  \
+  size_t rhsSize = ARRAY_SIZE(rhs);                  \
+  assert(lhsSize == rhsSize);                        \
+  for (size_t index = 0; index < lhsSize; ++index) { \
+    assert(!__VA_ARGS__(lhs[index], rhs[index]));    \
+  }                                                  \
+} while (0)
