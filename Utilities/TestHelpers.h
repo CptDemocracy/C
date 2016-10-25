0d4282bcdd68d08de08aa10f1cc7e24950cbf3ae
Efim Bogdanovsky <profit234@gmail.com>
2016-10-26 00:13:17 +0300

    Update TestHelpers.h

--- Utilities/TestHelpers.h
+++ Utilities/TestHelpers.h
@@ -9,7 +9,7 @@ do {                                                 \
   size_t rhsSize = ARRAY_SIZE(rhs);                  \
   assert(lhsSize == rhsSize);                        \
   for (size_t index = 0; index < lhsSize; ++index) { \
-    assert(lhs[index] == rhs[index]);                \
+    assert((lhs)[index] == (rhs)[index]);            \
   }                                                  \
 } while (0)
 
@@ -19,20 +19,20 @@ do {                                                 \
   size_t rhsSize = ARRAY_SIZE(rhs);                  \
   assert(lhsSize == rhsSize);                        \
   for (size_t index = 0; index < lhsSize; ++index) { \
-    assert(!cmp(lhs[index], rhs[index]));            \
+    assert(!cmp((lhs)[index], (rhs)[index]));        \
   }                                                  \
 } while (0)
 
 #define ASSERT_DYNAMIC_ARRAY_EQUALS(lhs, lhsSize, rhs, rhsSize)   \
  do {                                                             \
   assert((lhs) && (rhs) || !(lhs) && !(rhs));                     \
-  if (lhs && rhs) {                                               \
+  if ((lhs) && (rhs)) {                                           \
     assert(lhsSize == rhsSize);                                   \
-    assert(sizeof(lhs[0]) == sizeof(rhs[0]));                     \
-    size_t itemSize = sizeof(lhs[0]);                             \
+    assert(sizeof((lhs)[0]) == sizeof((rhs)[0]));                 \
+    size_t itemSize = sizeof((lhs)[0]);                           \
     for (size_t index = 0; index < lhsSize; ++index) {            \
-      const void **leftItem = (char*)lhs + index * itemSize;      \
-      const void **rightItem = (char*)rhs + index * itemSize;     \
+      const void **leftItem = (char*)(lhs) + index * itemSize;    \
+      const void **rightItem = (char*)(rhs) + index * itemSize;   \
       assert(!memcmp(leftItem, rightItem, itemSize));             \
       }                                                           \
   }                                                               \
@@ -41,13 +41,13 @@ do {                                                 \
 #define ASSERT_DYNAMIC_ARRAY_EQUALS_COMPARE_FN(lhs, lhsSize, rhs, rhsSize, cmp)  \
  do {                                                                            \
   assert((lhs) && (rhs) || !(lhs) && !(rhs));                                    \
-  if (lhs && rhs) {                                                              \
+  if ((lhs) && (rhs)) {                                                          \
     assert(lhsSize == rhsSize);                                                  \
-    assert(sizeof(lhs[0]) == sizeof(rhs[0]));                                    \
-    size_t itemSize = sizeof(lhs[0]);                                            \
+    assert(sizeof(lhs[0]) == sizeof((rhs)[0]));                                  \
+    size_t itemSize = sizeof((lhs)[0]);                                          \
     for (size_t index = 0; index < lhsSize; ++index) {                           \
-      const void **leftItem = (char*)lhs + index * itemSize;                     \
-      const void **rightItem = (char*)rhs + index * itemSize;                    \
+      const void **leftItem = (char*)(lhs) + index * itemSize;                   \
+      const void **rightItem = (char*)(rhs) + index * itemSize;                  \
       assert(!cmp(*leftItem, *rightItem));                                       \
     }                                                                            \
   }                                                                              \
