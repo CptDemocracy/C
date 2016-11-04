deb3c768b16870bc8fc080e37443ba7fa96d2c35
Efim Bogdanovsky <profit234@gmail.com>
2016-11-04 19:53:04 +0200

    Create String.h

---
+++ HelperClasses/String.h
@@ -0,0 +1,20 @@
+#ifndef STRING_H
+#define STRING_H
+
+  struct String {
+	struct List *chars;  
+  };
+
+  struct String *StringNew(struct String *self, const char *source);
+
+  void StringDispose(struct String *self);
+
+  void StringAdd(struct String *self, const struct String *other);
+
+  char StringGetAt(const struct String *self, int index);
+
+  int StringGetLength(const struct String *self);
+
+  void StringSlice(const struct String *self, int start, int end, int step, struct String *destination);
+
+#endif /* STRING_H */
