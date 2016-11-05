e6d4f0997f32e25545b69189c1b589574e1fb31c
Efim Bogdanovsky <profit234@gmail.com>
2016-11-05 04:22:31 +0200

    Update RandomUtilities.h

--- HelperClasses/RandomUtilities.h
+++ HelperClasses/RandomUtilities.h
@@ -10,4 +10,8 @@ void RandomShuffle(struct RandomGenerator *generator, void *items, size_t itemCo
 
 void *RandomChoice(struct RandomGenerator *generator, const void *items, size_t itemCount, size_t itemSize);
 
+void RandomGenerateString(struct RandomGenerator *generator, char *buffer, size_t stringLength, const char *charsSelection);
+
+int RandomGetInt(struct RandomGenerator *generator, int minValue, int maxValue);
+
 #endif /* RANDOM_UTILITIES_H */
